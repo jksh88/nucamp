9c5e3da4b660da9502d9b2f5f1c8ab8e4d70acb1
Joohoon Kim <jayjhkim7@gmail.com>
2021-01-27 12:53:10 -0500

    add directory component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">nucamp</NavbarBrand>
-        </div>
+        <NavbarBrand href="/">nucamp</NavbarBrand>
       </Navbar>
+      <div className="container">
+        <Directory />
+      </div>
     </div>
   );
 }
